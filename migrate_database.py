b81b60adb82e90c1d29c11c9647974686bc95763
Haydar Oleyat <online@oleyat.no>
2026-03-16 12:05:30 +0100

    Primary & foreign keys lagt til i migrate database

--- migrate_database.py
+++ migrate_database.py
@@ -12,7 +12,7 @@ cur = conn.cursor()
 cur.execute(
     """
     create table lego_set(
-        id text not null,
+        id text not null primary key,
         name text not null,
         year int null,
         category text null,
@@ -23,7 +23,7 @@ cur.execute(
 cur.execute(
     """
     create table lego_brick(
-        brick_type_id text not null,
+        brick_type_id text not null primary key,
         color_id int not null,
         name text not null,
         preview_image_url text null
@@ -33,8 +33,9 @@ cur.execute(
 cur.execute(
     """
     create table lego_inventory(
-        set_id text not null,
-        brick_type_id text not null,
+        inventory_id text not null primary key SERIAL,
+        set_id text not null foreign key,
+        brick_type_id text not null foreign key,
         color_id int not null,
         count int not null
     );
