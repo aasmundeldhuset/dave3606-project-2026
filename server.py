156eec022d775492650dbe573a0a4c90cd7769d5
abdallah <abali8460@oslomet.no>
2026-03-18 13:01:33 +0100

    added a message

--- server.py
+++ server.py
@@ -38,6 +38,7 @@ def sets():
                 for row in cur.fetchall():
                     html_safe_id = html.escape(row[0])
                     html_safe_name = html.escape(row[1])
+                    #what i changed
                     rows += f'<tr><td><a href="/set?id={html_safe_id}">{html_safe_id}</a></td><td>{html_safe_name}</td></tr>\n'
             print(f"Time to render all sets: {perf_counter() - start_time}")
     finally:
