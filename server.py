5bf5c49937c77f6b84935d21372226f9127cf371
malenalisken <malenalisken@outlook.com>
2026-03-23 14:38:32 +0100

    Changed rows to list to avoid painters algorithm

--- server.py
+++ server.py
@@ -24,25 +24,23 @@ def index():
 @app.route("/sets")
 def sets():
     template = open("templates/sets.html").read()
-    rows = ""
+    rows = []
 
     start_time = perf_counter()
     conn = psycopg.connect(**DB_CONFIG)
     try:
         with conn.cursor() as cur:
-            cur.execute("select id, name from lego_set order by id")
+            cur.execute("SELECT id, name FROM lego_set ORDER BY id ")
             for row in cur.fetchall():
                 html_safe_id = html.escape(row[0])
                 html_safe_name = html.escape(row[1])
-                existing_rows = rows
-                rows = existing_rows + f'<tr><td><a href="/set?id={html_safe_id}">{html_safe_id}</a></td><td>{html_safe_name}</td></tr>\n'
+                rows.append( f'<tr><td><a href="/set?id={html_safe_id}">{html_safe_id}</a></td><td>{html_safe_name}</td></tr>\n')
         print(f"Time to render all sets: {perf_counter() - start_time}")
     finally:
         conn.close()
 
-    page_html = template.replace("{ROWS}", rows)
+    page_html = template.replace("{ROWS}", "".join(rows))
     return Response(page_html, content_type="text/html")
-
 
 @app.route("/set")
 def legoSet():  # We don't want to call the function `set`, since that would hide the `set` data type.
